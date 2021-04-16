9ba452da65662accab8362810238525b9dc520b0
laurent aubourg <laurent.aubourg@free.fr>
2021-04-16 11:04:21 +0200

    reactivation des liens

--- swift Doc.playground/Pages/main Page.xcplaygroundpage/Contents.swift
+++ swift Doc.playground/Pages/main Page.xcplaygroundpage/Contents.swift
@@ -2,33 +2,30 @@
 /*:
  - Author: Laurent Aubourg
  **/
-import UIKit
+
 import PlaygroundSupport
-    /*:
- # Table des matières
  
- - - -
- * [init](init)
- * [les protocoles](Protocoles)
- * [les propriétés calculées](proprietes%20calculees)
- * [les observateurs](observateurs)
- * [les fonctions](fonctions)
- * [les fonctions : type et return type](typeReturnType)
- * [retours multiples](retours%20multiples)
- * [paramètres variatic](variatics)
- * [paramètres in-out](inOut)
- * [switch case](switchCase)
- * [Do , Catch, Try et Throw](doCatchTryThrow)
- * [les enum](enum)
- * [les enum : recursion](enum-recursion)
- * [Notification Center](notificationCenter)
- * [Les closures](closures)
- * [Calculated Tupple](calculatedTupple)
- * [Les boucles](Les%20boucles)
- * [Les optionals](Les%20optionals)
- * [Les ranges](range)
-*/
-var c = maClasse()
-c.test
-//: [suite](@next)
+/*:
+ ## Table des matières
+  */
+//: * [init](init)
+//: * [les protocoles](Protocoles)
+//: * [les propriétés calculées](proprietes%20calculees)
+//: * [les observateurs](observateurs)
+//: * [les fonctions](fonctions)
+//: * [les fonctions : type et return type](typeReturnType)
+//: * [retours multiples](retours%20multiples)
+//: * [paramètres variatic](variatics)
+//: * [paramètres in-out](inOut)
+//: * [switch case](switchCase)
+//: * [Do , Catch, Try et Throw](doCatchTryThrow)
+//: * [les enum](enum)
+//: * [les enum : recursion](enum-recursion)
+//: * [Notification Center](notificationCenter)
+//: * [Les closures](closures)
+//: * [Calculated Tupple](calculatedTupple)
+//: * [Les boucles](Les%20boucles)
+//: * [Les optionals](Les%20optionals)
+//: * [Les ranges](range)
+//: * [suite](@next)
 
